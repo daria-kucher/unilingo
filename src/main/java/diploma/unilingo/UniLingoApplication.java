b507cffa289d0b967108c44a9636a6e9949a11e1
dariakucher <dariakucher@ukr.net>
2026-04-18 20:56:55 +0300

    Initial commit

---
+++ src/main/java/diploma/unilingo/UniLingoApplication.java
@@ -0,0 +1,13 @@
+package diploma.unilingo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniLingoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UniLingoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/diploma/unilingo/UniLingoApplicationTests.java
@@ -0,0 +1,13 @@
+package diploma.unilingo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniLingoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
