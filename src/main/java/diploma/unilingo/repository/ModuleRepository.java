3454b5eda53343fed7a25069bcd02832348de93c
dariakucher <dariakucher@ukr.net>
2026-04-19 17:42:34 +0300

    Add JPA repositories layer

---
+++ src/main/java/diploma/unilingo/repository/ModuleRepository.java
@@ -0,0 +1,7 @@
+package diploma.unilingo.repository;
+
+import diploma.unilingo.entity.Module;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ModuleRepository extends JpaRepository<Module, Long> {
+}
---
+++ src/main/java/diploma/unilingo/repository/SubSkillRepository.java
@@ -0,0 +1,7 @@
+package diploma.unilingo.repository;
+
+import diploma.unilingo.entity.SubSkill;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SubSkillRepository extends JpaRepository<SubSkill, Long> {
+}
