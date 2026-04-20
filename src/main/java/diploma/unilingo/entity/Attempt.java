4cdbdc9937e418dc696599fc46e1fd4f9702e4c5
dariakucher <dariakucher@ukr.net>
2026-04-20 17:49:27 +0300

    Add AttemptService and its implementation. Test it

--- src/main/java/diploma/unilingo/entity/Attempt.java
+++ src/main/java/diploma/unilingo/entity/Attempt.java
@@ -21,4 +21,52 @@ public class Attempt {
     private double timeSpent;
 
     private LocalDateTime timestamp;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Exercise getExercise() {
+        return exercise;
+    }
+
+    public void setExercise(Exercise exercise) {
+        this.exercise = exercise;
+    }
+
+    public boolean isCorrect() {
+        return correct;
+    }
+
+    public void setCorrect(boolean correct) {
+        this.correct = correct;
+    }
+
+    public double getTimeSpent() {
+        return timeSpent;
+    }
+
+    public void setTimeSpent(double timeSpent) {
+        this.timeSpent = timeSpent;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(LocalDateTime timestamp) {
+        this.timestamp = timestamp;
+    }
 }
