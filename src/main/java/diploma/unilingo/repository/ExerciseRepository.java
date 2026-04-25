9f240c734852b104456ccfee98af6b6d4903587e
dariakucher <dariakucher@ukr.net>
2026-04-25 20:35:28 +0300

    Fix ExerciseRepository method

--- src/main/java/diploma/unilingo/repository/ExerciseRepository.java
+++ src/main/java/diploma/unilingo/repository/ExerciseRepository.java
@@ -3,10 +3,17 @@ package diploma.unilingo.repository;
 import diploma.unilingo.entity.Exercise;
 import diploma.unilingo.entity.SubSkill;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface ExerciseRepository extends JpaRepository<Exercise, Long> {
 
-    List<Exercise> findBySubSkills(SubSkill target);
+    @Query("""
+    SELECT e FROM Exercise e
+    JOIN e.exerciseSubSkills ess
+    WHERE ess.subSkill = :subSkill
+""")
+    List<Exercise> findBySubSkills(@Param("subSkill") SubSkill subSkill);
 }
