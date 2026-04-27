ce40e6188e4ccae1cd2a60c46750819cc0179a43
dariakucher <dariakucher@ukr.net>
2026-04-27 12:26:24 +0300

    Install Bootstrap

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import LoginPage from "./pages/LoginPage.jsx";
+import RegisterPage from "./pages/RegisterPage.jsx";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+      <RegisterPage />
+
   </StrictMode>,
 )
