ac63fdb1cd68a1751f3d4e726917d5cacf9ebc3d
dariakucher <dariakucher@ukr.net>
2026-04-28 20:47:05 +0300

    Implement routing

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { BrowserRouter } from "react-router-dom";
+import {BrowserRouter, RouterProvider} from "react-router-dom";
 import App from './App.jsx'
 import LoginPage from "./pages/LoginPage.jsx";
 import RegisterPage from "./pages/RegisterPage.jsx";
@@ -9,11 +9,10 @@ import {Header} from "./components/Header.jsx";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import {Footer} from "./components/Footer.jsx";
 import HomePage from "./pages/HomePage.jsx";
+import router from "./routes.js";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-      <BrowserRouter>
-          <HomePage />
-      </BrowserRouter>
+      <RouterProvider router={router} />
   </StrictMode>,
 )
--- frontend/src/pages/App.jsx
+++ frontend/src/pages/App.jsx
@@ -5,14 +5,7 @@ import HomePage from "./HomePage.jsx";
 //import HomePage from "../pages/HomePage";
 
 function App() {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<HomePage />} />
-                <Route path="/login" element={<LoginPage />} />
-            </Routes>
-        </BrowserRouter>
-    );
+
 }
 
 export default App;
