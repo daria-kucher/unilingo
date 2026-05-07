e748cf78672edbdb890d8a414efc4a83ba9a74c1
dariakucher <dariakucher@ukr.net>
2026-05-07 17:10:35 +0300

    Update Course class with Lombok and Jakarta annotations

--- src/main/java/diploma/unilingo/entity/Course.java
+++ src/main/java/diploma/unilingo/entity/Course.java
@@ -1,31 +1,24 @@
 package diploma.unilingo.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.Setter;
 
+@Getter
+@Setter
 @Entity
+@Table(name = "course")
 public class Course {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
 
-    private String name; // General English
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
+    @NonNull
+    @NotBlank(message = "Name is required")
+    @Size(max = 255, message = "Name must be less than 255 characters")
+    private String name;
 }
