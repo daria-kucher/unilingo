c526f7efb3971e4bb6e6b1ab1dcdb85e535ed890
dariakucher <dariakucher@ukr.net>
2026-05-09 15:55:28 +0300

    Update LanguageLevel class with Lombok and Jakarta annotations

--- src/main/java/diploma/unilingo/entity/LanguageLevel.java
+++ src/main/java/diploma/unilingo/entity/LanguageLevel.java
@@ -1,41 +1,35 @@
 package diploma.unilingo.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.*;
 
+@Getter
+@Setter
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "language_level",
+        uniqueConstraints = @UniqueConstraint(columnNames = "name"))
 public class LanguageLevel {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
 
-    private String name; // A1, A2...
+    @NonNull
+    @NotBlank(message = "Name is required")
+    @Size(max = 255, message = "Name must be less than 255 characters")
+    @Column(name = "name", unique = true)
+    private String name;
 
-    private double threshold; // мінімальні бали
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public double getThreshold() {
-        return threshold;
-    }
-
-    public void setThreshold(double threshold) {
-        this.threshold = threshold;
-    }
+    @Min(value = 0, message = "Threshold cannot be less than 0")
+    @Max(value = 6000, message = "Threshold cannot be greater than 6000")
+    @Column(name = "threshold")
+    private double threshold;
 }
