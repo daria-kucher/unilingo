51c1a18023ef388a907fff8f84a3dd5058b65312
dariakucher <dariakucher@ukr.net>
2026-05-10 15:35:57 +0300

    Add Answer class. Update Exercise class with Lombok and Jakarta annotations

---
+++ src/main/java/diploma/unilingo/entity/Answer.java
@@ -0,0 +1,28 @@
+package diploma.unilingo.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "answer")
+public class Answer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @NonNull
+    @NotBlank(message = "Answer text is required")
+    @Column(name = "text")
+    private String text;
+
+    @Column(name = "is_correct")
+    private boolean correct;
+}
--- src/main/java/diploma/unilingo/entity/Exercise.java
+++ src/main/java/diploma/unilingo/entity/Exercise.java
@@ -1,19 +1,35 @@
 package diploma.unilingo.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
 
 import java.util.List;
 
+@Getter
+@Setter
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "exercise")
 public class Exercise {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
 
+    @NonNull
+    @NotBlank(message = "Question is required")
+    @Column(name = "question", columnDefinition = "TEXT")
     private String question;
 
-    private String correctAnswer;
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinColumn(name = "exercise_id")
+    private List<Answer> answers;
 
-    @ElementCollection
-    private List<String> wrongAnswers;
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "profile_id")
+    private SubSkillProfile profile;
 }
